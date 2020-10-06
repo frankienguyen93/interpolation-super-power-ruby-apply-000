23862fba9ff0beeb07aba64d8a6fec286ee2087b
Chi Nguyen <nguyenchiftu2101@gmail.com>
2020-10-06 02:58:36 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,12 @@
 # Write your #display_rainbow method here
 def display_rainbow(array_color)
   puts "R: ${array_color[0]}"
+  puts "R: ${array_color[1}"
+  puts "R: ${array_color[2}"
+  puts "R: ${array_color[3}"
+  puts "R: ${array_color[4}"
+  puts "R: ${array_color[5}"
+  puts "R: ${array_color[6}"
 
   
 end
