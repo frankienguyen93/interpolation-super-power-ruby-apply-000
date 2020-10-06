f4abc113fe93302f20df3c96efbd3f005dd98b21
Chi Nguyen <nguyenchiftu2101@gmail.com>
2020-10-06 02:57:49 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(array_color)
-  puts "R: #{letters[1]}"
-  puts "The second letter in the alphabet is: #{letters[0]}"
-  puts "The third letter in the alphabet is: #{letters[2]}"
+  puts "R: ${array_color[0]}"
+
   
 end
